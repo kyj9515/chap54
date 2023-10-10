c2c5b898792743fe1c0dccd9f0958c9b766cceb5
kyj9515 <ydh981@naver.com>
2023-10-10 15:51:29 +0900

    rec

--- src/main/java/com/example/controller/RectController.java
+++ src/main/java/com/example/controller/RectController.java
@@ -5,13 +5,10 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.util.Alpha;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
 
 @Controller
 public class RectController {
